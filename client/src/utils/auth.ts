c9e593ce0cf94fe15d708d8f5a26b90b0622cfce
hongfetti <ryan.hong1994@gmail.com>
2025-03-11 13:20:39 -0400

    application working as intended

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,16 +3,26 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<JwtPayload>(this.getToken())
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
     const token = this.getToken();
-    return token
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
+      }
+    } catch (err) {
+      return false
+    }
   }
 
   getToken(): string {
